79453e604c49dd9843abfc42f1528aae3d483e67
landerfuste29 <35373958+landerfuste29@users.noreply.github.com>
2018-01-15 17:02:49 +0100

    Start to MVVM
    Hemos creado el MVVM

---
+++ Calendar_Proyecto/Calendar_Proyecto/ViewModel/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar_Proyecto.ViewModel
+{
+    class LoginViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //AuthorBookEntities ctx=new AuthorBookEntities();
+
+    }
+}
