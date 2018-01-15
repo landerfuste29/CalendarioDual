67fceff7c14bba8d15da3ff6f8b22c33de9c6f59
Iven97 <35373808+Iven97@users.noreply.github.com>
2018-01-15 14:45:53 +0100

    Boton en tutorial

--- BombermanFinalComentado/SWscorpiaM15/SWscorpiaM15/FormTutorial.Designer.cs
+++ BombermanFinalComentado/SWscorpiaM15/SWscorpiaM15/FormTutorial.Designer.cs
@@ -39,6 +39,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pbTutMov)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbTecE)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -145,11 +146,21 @@
             this.label6.TabIndex = 9;
             this.label6.Text = "I si has agafat alguna bomba podràs posar-les davant teu amb la";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(234, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 54);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // FormTutorial
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(744, 556);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.pbTecE);
             this.Controls.Add(this.label6);
@@ -184,5 +195,6 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
     }
 }
