7162aa96e855496f666eeee64a2d4cd79e8b68d5
Iven97 <35373808+Iven97@users.noreply.github.com>
2018-01-25 14:56:07 +0100

    espero que no pete

---
+++ ProyectoCalendar/ProyectoCalendar/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,33 @@
+﻿using GalaSoft.MvvmLight.Command;
+using MvvmDialogs.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ProyectoCalendar.ViewModel
+{
+    class RegisterViewModel : IUserDialogViewModel
+    {
+        public virtual bool IsModal { get { return true; } }
+        public event EventHandler DialogClosing;
+
+        public void RequestClose()
+        {
+            this.DialogClosing(this, null);
+        }
+
+        public ICommand CloseCommand
+        {
+            get { return new RelayCommand(RequestClose); }
+        }
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+    }
+}
