28e314f9b0c49ce84363a8029eadd45b1e3aab7a
IvanLopezGonzalez <35375290+IvanLopezGonzalez@users.noreply.github.com>
2018-01-26 15:10:48 +0100

    Cambios_calendario

---
+++ ProyectoCalendar/ProyectoCalendar/ViewModel/CalendarioViewModel.cs
@@ -0,0 +1,55 @@
+﻿using GalaSoft.MvvmLight.Command;
+using MvvmDialogs.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ProyectoCalendar.ViewModel
+{
+    class CalendarioViewModel : IUserDialogViewModel
+    {
+        private ObservableCollection<IDialogViewModel> _Dialogs = new ObservableCollection<IDialogViewModel>();
+        public event PropertyChangedEventHandler PropertyChanged;
+        public System.Collections.ObjectModel.ObservableCollection<IDialogViewModel> Dialogs { get { return _Dialogs; } }
+
+        public virtual bool IsModal { get { return true; } }
+        public event EventHandler DialogClosing;
+
+        public void RequestClose()
+        {
+            this.DialogClosing(this, null);
+        }
+
+        public ICommand CloseCommand
+        {
+            get { return new RelayCommand(RequestClose); }
+        }
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        //  public class GoToAddEvent
+        public void calendarioView()
+        {
+            this.Dialogs.Add(new NewEventViewModel()
+            {
+                //  Title = "Esto es una mierda"
+
+            });
+        }
+
+        public ICommand GoToAddEvent
+        {
+            get { return new RelayCommand(calendarioView); }
+        }
+    }
+}
