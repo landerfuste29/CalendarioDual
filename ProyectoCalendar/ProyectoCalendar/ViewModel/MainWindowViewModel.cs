e50fe6d56a1a6a834f38cb1458896fb0bab29979
Iven97 <35373808+Iven97@users.noreply.github.com>
2018-02-05 14:22:07 +0100

    pegar

---
+++ ProyectoCalendar/ProyectoCalendar/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,196 @@
+﻿using GalaSoft.MvvmLight.Command;
+using MvvmDialogs.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyectoCalendar.Model;
+using System.Windows.Input;
+using System.Security.Cryptography;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+
+namespace ProyectoCalendar.ViewModel
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+        Usuario userLoginApp = new Usuario();
+
+        private ObservableCollection<IDialogViewModel> _Dialogs = new ObservableCollection<IDialogViewModel>();
+        CalendarEntities db = new CalendarEntities();
+
+        /*private Usuario _usrToLogin;
+        public Usuario UsrToLogin
+        {
+            get { return (_usrToLogin); }
+            set { _usrToLogin = value; NotifyPropertyChanged(); }
+        }*/
+        private string _mail;
+
+        public string Mail
+        {
+            get { return (_mail); }
+            set { _mail = value; NotifyPropertyChanged(); }
+        }
+
+        private string _password;
+
+        public string Password
+        {
+            get { return (_password); }
+            set { _password = value; NotifyPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public System.Collections.ObjectModel.ObservableCollection<IDialogViewModel> Dialogs { get { return _Dialogs; } }
+        public void registreView()
+        {
+            Usuario usu_aux = new Usuario();
+            Entidad ent_aux = new Entidad();
+            String mensToEnv;
+            this.Dialogs.Add(new RegisterViewModel()
+            {
+                //  Title = "Esto es una mierda"
+                EntidadToAdd = ent_aux,
+                UsrToRegist = usu_aux,
+
+                TextEnabled = true,
+                OnAdd = (sender) =>
+                {
+                    Usuario usEmExist = db.Usuarios.Where(w => w.email.Equals(usu_aux.email)).FirstOrDefault();
+                    Entidad entExist = db.Entidads.Where(w => w.nombre.Equals(ent_aux.nombre)).FirstOrDefault();
+
+                    if (usEmExist == null)
+                    {
+                        mensToEnv = "Todo OK";
+
+                        try
+                        {
+                            if (entExist == null)
+                            {
+                                db.Entidads.Add(ent_aux);
+                                usu_aux.Entidad_idEntidad = ent_aux.idEntidad;
+                            }
+                            else
+                            {
+                                usu_aux.Entidad_idEntidad = entExist.idEntidad;
+                            }
+
+                            usu_aux.contraseña = GetMd5Hash(usu_aux.contraseña);
+                            db.Usuarios.Add(usu_aux);
+                            db.SaveChanges();
+                        }
+                        catch (Exception e)
+                        {
+                        }
+                    }
+                    else
+                    {
+                        mensToEnv = "NO OK!!";
+
+                    }
+                    //MostrarMensaje
+                    sender.Close();
+                }
+
+            });
+        }
+        public void CalendarioView()
+        {
+            try
+            {
+
+
+                Usuario usEmExist = db.Usuarios.Where(w => w.email.Equals(Mail)).FirstOrDefault();
+
+                if (usEmExist != null)
+                {
+                    if (VerifyMd5Hash(Password, usEmExist.contraseña))
+                    {
+                        userLoginApp = usEmExist;
+                        this.Dialogs.Add(new CalendarioViewModel()
+                        {
+                            UserCalApp = userLoginApp
+                            //  Title = "Esto es una mierda"
+                        });
+                    }
+                    else
+                    {
+                        //Mensaje Password incorrectaUsrToLogin
+                    }
+                }
+                else
+                {
+                    //Mensaje El correo no existe
+                }
+            }
+            catch { }
+        }
+
+
+
+        public ICommand GoToRegister
+        {
+            get { return new RelayCommand(registreView); }
+        }
+        public ICommand GoToCalendario
+        {
+
+            get { return new RelayCommand(CalendarioView); }
+        }
+
+        static string GetMd5Hash(string input)
+        {
+            MD5 md5Hash = MD5.Create();
+
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            StringBuilder sBuilder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+        /*.................................................
+        Metode per comprobar MD5
+        ------------------------------------------*/
+        static bool VerifyMd5Hash(string input, string hash)
+        {
+            /*
+             input = Password Login
+             hash = Password BBDD
+             */
+
+            MD5 md5Hash = MD5.Create();
+            string hashOfInput = GetMd5Hash(input);
+
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        protected void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
---
+++ ProyectoCalendar/ProyectoCalendar/ViewModel/NewEventViewModel.cs
@@ -0,0 +1,224 @@
+﻿using GalaSoft.MvvmLight.Command;
+using MvvmDialogs.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ProyectoCalendar.Model;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+
+namespace ProyectoCalendar.ViewModel
+{
+    class NewEventViewModel : IUserDialogViewModel
+    {
+        private Usuario _usrRegist;
+        public Usuario UsrRegist
+        {
+            get { return (_usrRegist); }
+            set { _usrRegist = value; NotifyPropertyChanged(); }
+        }
+
+
+        private Evento _newEvento;
+
+        public Evento NewEvento
+        {
+            get
+            {
+                return _newEvento;
+            }
+            set
+            {
+                _newEvento = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+
+        private List<TipoEvento> _tipEventos;
+        private TipoEvento _selectedEvento;
+
+        public List<TipoEvento> TipEventos
+        {
+            get
+            {
+                return _tipEventos;
+            }
+            set
+            {
+                _tipEventos = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public TipoEvento SelectedEvento
+        {
+            get
+            {
+                return _selectedEvento;
+            }
+            set
+            {
+                _selectedEvento = value;
+                FillTipoEvID();
+                NotifyPropertyChanged();
+            }
+        }
+
+        public ICommand AddEventCommand
+        {
+            get { return new RelayCommand(AddEvent); }
+        }
+
+        private DateTime _chosenDate = new DateTime();
+
+        public DateTime ChosenDate
+        {
+            get { return _chosenDate; }
+            set
+            {
+                _chosenDate = value;
+            }
+        }
+        private void FillTipoEvID()
+        {
+            
+            NewEvento.TipoEvento_idTipoEvento = SelectedEvento.idTipoEvento;
+        }
+
+
+        public void AddEvent()
+        {
+            
+
+            if (this.OnAdd != null)
+                this.OnAdd(this);
+            else
+                Close();
+
+        }
+        public Action<NewEventViewModel> OnAdd { get; set; }
+        public void Close()
+        {
+            
+            if (this.DialogClosing != null)
+                this.DialogClosing(this, new EventArgs());
+
+        }
+
+        public virtual bool IsModal { get { return true; } }
+        public event EventHandler DialogClosing;
+
+        public void RequestClose()
+        {
+            
+            this.DialogClosing(this, null);
+        }
+
+        public ICommand CloseCommand
+        {
+            get { return new RelayCommand(RequestClose); }
+        }
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+
+
+        private void FillHora()
+        {
+
+            TimeSpan hora = new TimeSpan(SelectedCboxHora, SelectedCboxMin, 0);
+            NewEvento.fecha = ChosenDate + hora;
+        }
+
+        private void FillMin()
+        {
+
+            TimeSpan hora = new TimeSpan(SelectedCboxHora, SelectedCboxMin, 0);
+            NewEvento.fecha = ChosenDate + hora;
+        }
+
+
+
+
+        private List<int> _cboxHora;
+        private int _selectedCboxHora;
+        public List<int> CboxHora
+        {
+            get
+            {
+                return _cboxHora;
+            }
+            set
+            {
+                _cboxHora = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public int SelectedCboxHora
+        {
+            get
+            {
+                return _selectedCboxHora;
+            }
+            set
+            {
+                _selectedCboxHora = value;
+                FillHora();
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        
+
+        private List<int> _cboxMin;
+        private int _selectedCboxMin;
+        public List<int> CboxMin
+        {
+            get
+            {
+                return _cboxMin;
+            }
+            set
+            {
+                _cboxMin = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public int SelectedCboxMin
+        {
+            get
+            {
+                return _selectedCboxMin;
+            }
+            set
+            {
+                _selectedCboxMin = value;
+                FillMin();
+                NotifyPropertyChanged();
+            }
+        }
+
+
+    }
+}
